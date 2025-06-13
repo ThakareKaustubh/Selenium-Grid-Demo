13baf03490ae39fdf6b47f5d0ca683d494c4bd11
thakarekaustubh <kaustubhmthakare@gmail.com>
2025-06-13 15:45:39 +0530

    updated test_grid.py with allure features

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -1,7 +1,14 @@
 import pytest
+import allure
 
+@allure.title("Verify Google Page Title")
+@allure.description("This test verifies that the title of the Google homepage contains 'Google'")
+@allure.severity(allure.severity_level.NORMAL)
 @pytest.mark.parametrize("driver", ["chrome", "firefox"], indirect=True)
 def test_google_title(driver):
-    driver.get("https://www.google.com")
-    assert "Google" in driver.title
+    with allure.step("Open Google homepage"):
+        driver.get("https://www.google.com")
+
+    with allure.step("Verify title contains 'Google'"):
+        assert "Google" in driver.title
 
